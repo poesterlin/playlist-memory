9e36136367a203a005c385e7d4728ab193335f37
Philip Oesterlin <philip.oesterlin+github@gmail.com>
2024-01-06 14:18:01 +0100

    remove node polyfil

--- main/vite.config.ts
+++ main/vite.config.ts
@@ -1,7 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), nodePolyfills()]
+	plugins: [sveltekit()]
 });
